881e4627e5a1e7e2a30035077c3e1e10540b3b1c
ymyldz <113609896+ymyldz@users.noreply.github.com>
2023-01-18 12:40:47 +0300

    Initial Commit

---
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySwiftUIApp.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Yusuf Mert Yıldız on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
